4490296faf43778fccb3ec45f6f0dc5fb7a54e5c
guney <guneyyilmaz2707@gmail.com>
2025-06-20 13:27:19 +0300

    remove old files

--- src/main/kotlin/TempMemory.kt
+++
@@ -1,7 +0,0 @@
-package net.guneyilmaz0.riotapi
-
-object TempMemory {
-    const val dataDragonURL = "https://ddragon.leagueoflegends.com/"
-
-    var latestLOLVersion: String? = null
-}
--- src/main/kotlin/objects/Account.kt
+++
@@ -1,7 +0,0 @@
-package net.guneyilmaz0.riotapi.objects
-
-data class Account(
-    val puuid: String,
-    val gameName: String,
-    val tagLine: String
-)
